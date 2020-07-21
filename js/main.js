351c0ef0f2aaeb97e6164c22f1f66512598e342d
Juan_213 <juanpagri2@hotmail.com>
2020-07-21 13:38:29 -0500

    Añadiendo validaciones validaciones

--- js/main.js
+++ js/main.js
@@ -3,34 +3,71 @@ function LlenarCiudades(s1,s2){
 	var s2 = document.getElementById(s2);
 	s2.innerHTML = "";
 	if(s1.value == "Antioquia"){
-		var optionArray = ["|","medellin|Medellín","envigado|Envigado","sabaneta|Sabaneta","bello|Bello"];
+		var ArregloOpciones = ["medellin|Medellín","envigado|Envigado","sabaneta|Sabaneta","bello|Bello"];
 	} else if(s1.value == "Valle del Cauca"){
-		var optionArray = ["|","cali|Cali","buga|Buga","palmira|Palmira","cartago|Cartago"];
+		var ArregloOpciones = ["cali|Cali","buga|Buga","palmira|Palmira","cartago|Cartago"];
 	} else if(s1.value == "Arauca"){
-		var optionArray = ["|","arauca|Arauca","arauquita|Arauquita","saravena|Saravena"];
+		var ArregloOpciones = ["arauca|Arauca","arauquita|Arauquita","saravena|Saravena"];
 	}else if(s1.value == "Cundinamarca"){
-		var optionArray = ["|","bituima|Bituima","chipaque|Chipaque","ubaque|Ubaque","cota|Cota"];
+		var ArregloOpciones = ["bituima|Bituima","chipaque|Chipaque","ubaque|Ubaque","cota|Cota"];
 	} else if(s1.value == "Magdalena"){
-		var optionArray = ["|","santa Marta|Santa Marta","algarrobo|Algarrobo","concordia|Concordia"];
+		var ArregloOpciones = ["santa Marta|Santa Marta","algarrobo|Algarrobo","concordia|Concordia"];
 	}
-	for(var opcion in optionArray){
-		var arreglo = optionArray[opcion].split("|");
-		var newOption = document.createElement("option");
-		newOption.value = arreglo[0];
-		newOption.innerHTML = arreglo[1];
-		s2.options.add(newOption);
+	for(var opcion in ArregloOpciones){
+		var arreglo = ArregloOpciones[opcion].split("|");
+		var nuevaOpcion = document.createElement("option");
+		nuevaOpcion.value = arreglo[0];
+		nuevaOpcion.innerHTML = arreglo[1];
+		s2.options.add(nuevaOpcion);
 	}
 }
 
-function comprobarClave(){
-    password = document.f1.password.value
-    password2 = document.f1.password2.value
+
+function validacion() {
+	password = document.f1.password.value;
+    password2 = document.f1.password2.value;
 
     if (password != password2){
        document.f1.password.value="";
        document.f1.password2.value="";
        alert("Las dos contraseñas son distintas...");
+       return false;
     }
-       
-   		
+
+	form = document.f1;
+    indice = document.getElementById("select-departamentos").selectedIndex;
+	if( indice == null || indice == 0 ) {
+		alert("Selecciona un departamento");
+        form.select-departamentos.focus();
+  		return false;
+	}
+	
+	if (form.nombre.value ==0 ){
+		form.nombre.value="";
+		form.nombre.focus();
+	}
+
+	if (form.apellidos.value ==0 ){
+		form.apellidos.value="";
+		form.apellidos.focus();
+	}
+
+	if (form.usuario.value ==0 ){
+		form.usuario.value="";
+		form.usuario.focus();
+	}
+
+	opciones = document.getElementsByName("genero");
+	var seleccionado = false;
+	for(var i=0; i<opciones.length; i++) {
+  		if(opciones[i].checked) {
+    		seleccionado = true;
+    	break;
+  		}
+	}
+
+	if(!seleccionado) {
+  		return false;
+	}
+
 }
--- scripts/script.php
+++ scripts/script.php
@@ -1,8 +1,12 @@
 <?php 
 	//echo $_POST['user']. "<br>";
 	//echo $_POST['password']. "<br>";
+	$nombre = $_POST['usuario'];
 	$salt = "randomstringforsalt";
 	$password = $_POST['password'];
-
 	$md5 = md5($salt.$password);
+
+	echo "Bienvenido $nombre <br> "; 
+	echo "Contraseña original $password <br>"; 
+	echo "Contraseña mod $md5";
 ?>
