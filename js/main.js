92bc8b79404ee3f299bd2bae773bddb1655f5f51
Xaca <and1res@hotmail.com>
2020-07-23 07:30:18 -0500

    ajustes de clase

--- js/main.js
+++ js/main.js
@@ -1,41 +1,49 @@
-function LlenarCiudades(s1,s2){
-	var s1 = document.getElementById(s1);
-	var s2 = document.getElementById(s2);
-	s2.innerHTML = "";
-	if(s1.value == "Antioquia"){
-		var ArregloOpciones = ["medellin|Medellín","envigado|Envigado","sabaneta|Sabaneta","bello|Bello"];
-	} else if(s1.value == "Valle del Cauca"){
-		var ArregloOpciones = ["cali|Cali","buga|Buga","palmira|Palmira","cartago|Cartago"];
-	} else if(s1.value == "Arauca"){
-		var ArregloOpciones = ["arauca|Arauca","arauquita|Arauquita","saravena|Saravena"];
-	}else if(s1.value == "Cundinamarca"){
-		var ArregloOpciones = ["bituima|Bituima","chipaque|Chipaque","ubaque|Ubaque","cota|Cota"];
-	} else if(s1.value == "Magdalena"){
-		var ArregloOpciones = ["santa Marta|Santa Marta","algarrobo|Algarrobo","concordia|Concordia"];
-	}
+let select_ciudad, select_departamento, genero;
+let departamentos = [];
+departamentos[0] = "Antioquia";
+
+let ciudades = [];
+ciudades[0] = ["medellin|Medellín","envigado|Envigado","sabaneta|Sabaneta","bello|Bello"];
+ciudades[1] = ["cali|Cali","buga|Buga","palmira|Palmira","cartago|Cartago"];
+ciudades[2] = ["arauca|Arauca","arauquita|Arauquita","saravena|Saravena"];
+ciudades[3] = ["bituima|Bituima","chipaque|Chipaque","ubaque|Ubaque","cota|Cota"];
+ciudades[4] = ["santa Marta|Santa Marta","algarrobo|Algarrobo","concordia|Concordia"];
+
+window.onload = ()=>{
+	select_departamentos = document.getElementById("select-departamentos");
+	select_ciudad = document.getElementById("select-ciudad");
+	genero = document.getElementsByName("genero");
+}
+
+function LlenarCiudades(){
+	let ArregloOpciones;
+	select_ciudad.innerHTML = "";
+	ArregloOpciones=ciudades[select_departamentos.value];
 	for(var opcion in ArregloOpciones){
 		var arreglo = ArregloOpciones[opcion].split("|");
 		var nuevaOpcion = document.createElement("option");
 		nuevaOpcion.value = arreglo[0];
 		nuevaOpcion.innerHTML = arreglo[1];
-		s2.options.add(nuevaOpcion);
+		select_ciudad.options.add(nuevaOpcion);
 	}
 }
 
 
 function validacion() {
-	password = document.f1.password.value;
-    password2 = document.f1.password2.value;
+	let password = document.f1.password;
+	let password2 = document.f1.password2;
+	let form = document.f1;
+    let indice = select_departamentos.selectedIndex;
+	let opciones = genero;
+	let seleccionado = false;
 
-    if (password != password2){
-       document.f1.password.value="";
-       document.f1.password2.value="";
+    if (password.value != password2.value){
+       password.value="";
+       password2.value="";
        alert("Las dos contraseñas son distintas...");
        return false;
     }
 
-	form = document.f1;
-    indice = document.getElementById("select-departamentos").selectedIndex;
 	if( indice == null || indice == 0 ) {
 		alert("Selecciona un departamento");
         form.select-departamentos.focus();
@@ -57,12 +65,10 @@ function validacion() {
 		form.usuario.focus();
 	}
 
-	opciones = document.getElementsByName("genero");
-	var seleccionado = false;
 	for(var i=0; i<opciones.length; i++) {
   		if(opciones[i].checked) {
     		seleccionado = true;
-    	break;
+    		break;
   		}
 	}
 
